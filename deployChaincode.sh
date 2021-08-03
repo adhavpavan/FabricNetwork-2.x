bc8cafa728aa4dfa6d43021a84268770a199d78d
Pavan <adhavpavan@gmail.com>
2021-08-03 21:25:58 +0530

    user mspconfig path functio added

--- deployChaincode.sh
+++ deployChaincode.sh
@@ -21,6 +21,13 @@ setGlobalsForPeer0Org1() {
     export CORE_PEER_ADDRESS=localhost:7051
 }
 
+setGlobalsForOrg1() {
+    export CORE_PEER_LOCALMSPID="Org1MSP"
+    export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG1_CA
+    export CORE_PEER_MSPCONFIGPATH=${PWD}/artifacts/channel/crypto-config/peerOrganizations/org1.example.com/users/User1@org1.example.com/msp
+    export CORE_PEER_ADDRESS=localhost:7051
+}
+
 setGlobalsForPeer0Org2() {
     export CORE_PEER_LOCALMSPID="Org2MSP"
     export CORE_PEER_TLS_ROOTCERT_FILE=$PEER0_ORG2_CA
@@ -250,6 +257,7 @@ chaincodeInvokeDeleteAsset() {
 
 chaincodeQuery() {
     setGlobalsForPeer0Org1
+    # setGlobalsForOrg1
     peer chaincode query -C $CHANNEL_NAME -n ${CC_NAME} -c '{"function": "GetCarById","Args":["1"]}'
 }
 
